9ce7c0fd392dc6652a330ad3420fe77818a6b5b3
JONES LUKORITO <JONESDEELDER@gmail.com>
2021-08-23 17:09:32 +0300

    move zeroes

---
+++ move_zeroes.py
@@ -0,0 +1,58 @@
+
+
+#*Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+#!Note that you must do this in-place without making a copy of the array.
+
+# nums = [0,2,3,0,7,5,0,9,0]
+
+# j=0
+
+# n = len(nums)
+# for num in nums:
+#     if (num != 0):
+#         nums[j] = num
+#         j+=1
+
+
+# for i in range (j, n):
+#     nums[i] = 0
+
+# print (nums)
+
+
+#*Attempt 2
+
+# nums = [0,2,3,0,7,5,0,9,0]
+
+# n = len (nums)
+
+# j= 0
+
+# for num in nums:
+#     if (num !=0):
+#         nums[j] = num
+#         j+=1
+
+# for i in range (j, n):
+#     nums[i] = 0
+
+# print (nums)
+
+#*Whiteboarding attempt 
+
+nums = [0,2,3,0,7,5,0,9,0]
+
+n = len (nums)
+
+j = 0
+
+for num in nums:
+    if (num!=0):
+        nums[j]= num
+        j+=1
+
+for i in range (j, n):
+    nums[i] = 0
+
+print (nums)
